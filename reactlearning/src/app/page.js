a2b989051f9a388cc99a9fc82b38254841a3bebc
kikealcocerdz <100472118@alumnos.uc3m.es>
2023-06-14 16:53:55 +0200

    twitterFollowCard

--- reactlearning/src/app/page.js
+++ reactlearning/src/app/page.js
@@ -1,5 +1,7 @@
 "use client"
 import { useState } from 'react'
+import './page.module.css'
+import styles from './page.module.css'
 
 
 const Button = ({handleClick, text}) => {
@@ -9,32 +11,55 @@ const Button = ({handleClick, text}) => {
     </button>
   )
 }
-       
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-
-  const handleClick = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
-
+const TwitterCard = ({formatuserName, userName, name, isFollowing}) => {
+  console.log(isFollowing)
   return (
-    <div>
-      <p>{anecdotes[selected]}</p>
-      <Button handleClick={handleClick} text="next anecdote"/>
-    </div>
-  )
+    <article className='tw-follow-card'>
+      <header className= 'tw-follow-card-header'>
+        <img className='tw-follow-card-img' alt= "Avatar" 
+        src = {`https://unavatar.io/${userName}`} />
+        <div>
+          <strong>{name}</strong>
+          <span>{formatuserName(userName)}</span>
+        </div>
+      </header>
+      <aside>
+        <Button text="Follow" handleClick={() => alert("followed")} />
+      </aside>
+    </article>
+    )
 }
 
+const App = () => {
+  const format = (userName)  => `@${userName}`
+  return (
+    <>
+      <section className='app'>
+      <TwitterCard 
+      formatuserName={format} 
+      userName="kikealcocerdz" 
+      name="Enrique Alcocer" 
+      isFollowing
+      />
+
+      <TwitterCard 
+      formatuserName={format} 
+      userName="midudev" 
+      name="Miguel Durán" 
+      isFollowing
+      />
+
+      <TwitterCard 
+      formatuserName={format} 
+      userName="midudev" 
+      name="Miguel Durán" 
+      isFollowing
+      />
+
+      </section>   
+    </>
+    )
+}
+  
 export default App
