f9c4d76840689e4720f2a5b264cbc725cda5617b
kikealcocerdz <100472118@alumnos.uc3m.es>
2023-06-29 13:13:59 +0200

    THIRDPROJECT

--- mouse-follower/src/App.jsx
+++ mouse-follower/src/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const FollowMouse = () =>{
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x:0, y:0})
+
+  useEffect(() =>{
+    console.log('effect', {enabled})
+    const handleMove = (event) =>{
+      const{clientX, clientY} = event
+      console.log('handleMove', {clientX, clientY})
+      setPosition({x: clientX, y:clientY})
+    }
+    if (enabled) {
+    window.addEventListener('pointermove', handleMove)
+    }
+    return () => {window.removeEventListener('pointermove', handleMove)
+                  setPosition({x:0, y:0})}
+  }, [enabled])
 
   return (
     <>
-      <div>
-      </div>
-    </>
+    <div style={
+    { position:'absolute',
+      backgroundColor: '#09f',
+      borderRadius: '50%',
+      opacity: 0.8,
+      pointerEvents: 'none',
+      left: -20,
+      top: -20,
+      width: 40,
+      height: 40,
+      transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+    <h3> proyect 3</h3>
+    <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Encendio' : 'Apagao'} seguir puntero</button>
+  </>
+  )
+    }
+
+function App () {
+  return (
+    <main>
+      <FollowMouse/>
+    </main>
   )
 }
 
